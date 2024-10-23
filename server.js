53d15e7fe7a98d7d43701b3f4af1158eff593a46
Hamidreza Navaei <HRNavaei@yahoo.com>
2024-10-23 18:29:42 +0330

    Feat: setup and use express

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app.js';
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
 
 mongoose
   .connect(process.env.DB_URI)
-  .then(() => console.log("Successfully connected to database."));
+  .then(() => console.log('Successfully connected to database.'));
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Listening to requests on port ${port}.`));
