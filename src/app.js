78418b3efc23f00615e60a99076014b96ceec660
Hamidreza Navaei <HRNavaei@yahoo.com>
2024-10-25 20:21:36 +0330

    Feat: Config error handling files in app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 
 import * as config from './config.js';
-import authController from './controller/auth-controller.js';
+
+import globalErrorHandler from './utils/global-error-handler.js';
 
 const app = express();
 
 app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use(globalErrorHandler);
 
 export default app;
