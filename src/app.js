6da50d70df3ea26f7fd879885530cf9c87f76d86
Hamidreza Navaei <HRNavaei@yahoo.com>
2024-10-25 16:50:20 +0330

    Feat: Use morgan in a middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 
 import * as config from './config.js';
+import authController from './controller/auth-controller.js';
 
 const app = express();
+
+app.use(morgan('dev'));
+
 export default app;
