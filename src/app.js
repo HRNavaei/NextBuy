923b825f0ff6f8159490a6474a7d21149bb8bfd1
Hamidreza Navaei <HRNavaei@yahoo.com>
2024-10-25 20:24:59 +0330

    Feat: Specify route for auth in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 
 import * as config from './config.js';
-
+import authRouter from './routes/auth-routes.js';
 import globalErrorHandler from './utils/global-error-handler.js';
 
 const app = express();
@@ -10,6 +10,8 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(express.json());
+
+app.use('/api/v1/auth', authRouter);
 
 app.use(globalErrorHandler);
 
