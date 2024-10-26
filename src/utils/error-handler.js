3d7ca39f58bee209b0b85340cd8444646ef09108
Hamidreza Navaei <HRNavaei@yahoo.com>
2024-10-26 22:30:00 +0330

    Feat: Develop the global error handler

--- src/utils/error-handler.js
+++
@@ -1,5 +0,0 @@
-export default (_function) => {
-  return (req, res, next) => {
-    _function(req, res, next).catch(next);
-  };
-};
--- src/utils/global-error-handler.js
+++ src/utils/global-error-handler.js
@@ -1,3 +1,44 @@
 export default (err, req, res, next) => {
+  const revisedErr = reviseError(err);
+
+  if (revisedErr.isOperational) {
+    const resObj = {
+      state: 'fail',
+      code: revisedErr.code,
+      message: revisedErr.message,
+    };
+
+    if (revisedErr.code === 'VALIDATION_ERROR') {
+      resObj.validationErrors = err.validationErrors;
+    }
+
+    return res.status(revisedErr.httpStatus).json(resObj);
+  }
+
+  if (revisedErr.isMongoError) return handleMongoError(revisedErr, res);
+
+  res.status(500).json({
+    state: 'error',
+    message: 'Internal server error',
+    error: err,
+  });
   console.log(err);
+};
+
+const reviseError = (err) => {
+  const revisedErr = { ...err };
+
+  if (err.name === 'MongoServerError') revisedErr.isMongoError = true;
+  if (err.name === 'ValidationEror') revisedErr.isValidationError = true;
+
+  return revisedErr;
+};
+
+const handleMongoError = (err, res) => {
+  if (err.code === 11000) {
+    res.status(400).json({
+      state: 'fail',
+      message: 'A user with provided email already existed',
+    });
+  }
 };
