0e77a7ba669e53a54d8efa4778e902453c9cabae
Hamidreza Navaei <HRNavaei@yahoo.com>
2024-10-26 22:09:48 +0330

    Feat: Add error handling and other improvements in signup function

--- src/controller/auth-controller.js
+++ src/controller/auth-controller.js
@@ -1,20 +1,35 @@
-import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import * as config from '../config.js';
 import User from '../model/user-model.js';
-import errorHandler from '../utils/error-handler.js';
+import handleAsyncError from '../utils/async-error-handler.js';
+import OperationalError from '../utils/operational-error.js';
 
-export const signup = errorHandler(async (req, res, next) => {
+export const signup = handleAsyncError(async (req, res, next) => {
   const { email, password } = req.body;
 
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  let user = null;
+  try {
+    user = await User.create({
+      email,
+      password,
+    });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      const validationErrorCodes = [];
 
-  const user = await User.create({
-    email,
-    password: hashedPassword,
-    createdAt: new Date(),
-  });
+      const validationErrorFields = Object.keys(err.errors);
+      for (const field of validationErrorFields) {
+        validationErrorCodes.push(err.errors[field].message);
+      }
+
+      throw new OperationalError('VALIDATION_ERROR', validationErrorCodes);
+    } else if (err.name === 'MongoServerError' && err.code === 11000) {
+      throw new OperationalError('DUP_USER');
+    }
+  }
+  user = user.toObject();
+  delete user.password;
 
   const token = jwt.sign({ id: user._id }, config.JWT_SECRET, {
     expiresIn: config.JWT_EXPIRE_AFTER,
